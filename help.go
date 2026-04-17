675cf8e39be042d98590bafeb96f3eff45ed8a0a
Yukthi C <54282625+x0Yukthi@users.noreply.github.com>
2026-04-16 22:48:00 -0700

    adding help

---
+++ help.go
@@ -0,0 +1,12 @@
+package main
+
+func getHelp() string {
+	return "COMMANDS: " +
+		"1. 'time.[city]' - Get local time. " +
+		"2. 'weather.[city]' - Get temp/humidity. " +
+		"3. 'pi.[sub]' - Use value, info, digits, formula, symbol, or fun." +
+		"4. 'country.[name]' - Get capital, region, population, currency." +
+		"5. 'convert.[amount].[from].[to]' - Currency conversion." +
+		"6. 'crypto.[coin]' - Live crypto price. " +
+		"7. 'ip.me' - Your IP info. "
+}
