1171000c564ef5981577c68e9798bfeed598de13
Yukthi C <54282625+x0Yukthi@users.noreply.github.com>
2026-04-16 22:48:34 -0700

    Update help

--- help.go
+++ help.go
@@ -4,7 +4,7 @@ func getHelp() string {
 	return "COMMANDS: " +
 		"1. 'time.[city]' - Get local time. " +
 		"2. 'weather.[city]' - Get temp/humidity. " +
-		"3. 'pi.[sub]' - Use value, info, digits, formula, symbol, or fun." +
+		"3. 'pi.[sub]' - Use value, info, digits, formula, or fun." +
 		"4. 'country.[name]' - Get capital, region, population, currency." +
 		"5. 'convert.[amount].[from].[to]' - Currency conversion." +
 		"6. 'crypto.[coin]' - Live crypto price. " +
